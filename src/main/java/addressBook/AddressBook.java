dca98f5e940692fc721292549a063c4608227b4a
DESKTOP-VFH6PON\USER <arpit@edupluscampus.com>
2023-04-07 19:14:49 +0530

    changes

--- src/main/java/addressBook/AddressBook.java
+++ src/main/java/addressBook/AddressBook.java
@@ -12,102 +12,28 @@ public class AddressBook {
 
 
     //------------------------Book Logic Start ---------------------
-    public static ArrayList<AddressBook> addressBook_arr = new ArrayList<>();
+    public static ArrayList<String> addressBook_arr = new ArrayList<String>();
     public static Hashtable<String, ArrayList<Contact>> addressBook_hashtable = new Hashtable<String, ArrayList<Contact>>();
     static void addAddressBook() {
 
         Scanner get = new Scanner(System.in);
         int getMoreBook;
-        int check = 1;
 
         do {
             System.out.println("\n       ~ ~ ~ Enter BOOK Name ~ ~ ~         ");
             String getBookName = get.next();
 
 // ---------- To Add AddressBook 2nd time (check)----------------------------------------------
-            for (AddressBook addBook : addressBook_arr) {
-                System.out.println("Nothing" + addBook);
+            for (String addBook : addressBook_arr) {
+//                System.out.println("Nothing" + addBook);
                 if (getBookName.equals(addBook)) {
-                    System.out.println(addBook + " AddressBook already present ");
-                    check = 0;
-                } else {
-                    AddressBook addressBook = new AddressBook();
-                    addressBook_arr.add(addressBook);
-                    System.out.println("Address Book Added ...");
-                    check = 0;
-
-                    //--------T0 add contact
-                    Scanner sc = new Scanner(System.in);
-                    System.out.println("\n             ~ ~ ~ ADD NEW CONTACT press 1-9  :: To EXIT ( 0 ZERO ) ~ ~ ~             ");
-                    int exit = sc.nextInt();
-                        while (exit != 0) {
-                            Scanner sc1 = new Scanner(System.in);
-                            System.out.print("Enter command \t\t( Add , Edit , Display , Searchcitystate ," +
-                                    "\t SortByName , SortByCity , SortByState , SortByZip , \n" +
-                                    "\t \t\t \t\t \tContactsByCity , ContactsByState , ContactCountByCity , ContactCountByState ," +
-                                    "WriteToFile , "+"ReadFromFile"+
-                                    "\t Delete , Exit ): \n");
-                            String command = sc1.nextLine();
-                            switch (command) {
-                                case "Add":
-                                    addressBook.addPersonDetails();
-                                    break;
-                                case "Edit":
-                                    addressBook.editPersonDetails();
-                                    break;
-                                case "Display":
-                                    addressBook.display();
-                                    break;
-                                case "Searchcitystate":
-                                    addressBook.searchDetailsByCityOrState();
-                                    break;
-                                case "SortByName":
-                                    addressBook.sortByName();
-                                    break;
-                                case "SortByCity":
-                                    addressBook.sortByCity();
-                                    break;
-                                case "SortByState":
-                                    addressBook.sortByState();
-                                    break;
-                                case "SortByZip":
-                                    addressBook.sortByZip();
-                                    break;
-                                case "ContactsByCity":
-                                    addressBook.getContactsByCity();
-                                    break;
-                                case "ContactsByState":
-                                    addressBook.getContactsByState();
-                                    break;
-                                case "ContactCountByCity":
-                                    addressBook.getContactCountByCity();
-                                    break;
-                                case "ContactCountByState":
-                                    addressBook.getContactCountByState();
-                                    break;
-                                case "WriteToFile":
-                                    addressBook.writeToFile();
-                                    break;
-                                case "ReadFromFile":
-                                    addressBook.readFromFile();
-                                    break;
-                                case "Delete":
-                                    addressBook.deletePersonDetails();
-                                    break;
-                                case "Exit":
-                                    break;
-//                                   addressBook.printContacts();
-                            }
-                            System.out.println("\n             ~ ~ ~ ADD NEW CONTACT press 1-9  ::  To EXIT press (0 ZERO) ~ ~ ~             ");
-                            exit = sc.nextInt();
-                        }
-                }
-            }
+                    System.out.println(" AddressBook already present ");
+                    return;
+                }}
 // ---------- To Add AddressBook 1st time (check)----------------------------------------------
-            if (check == 1) {
                 AddressBook addressBook = new AddressBook();
-                addressBook_arr.add(addressBook);
-                System.out.println("Address Book Added ...");
+                addressBook_arr.add(getBookName);
+                System.out.println("Address Book Added ...2"+addressBook_arr);
 
                 //--------T0 add contact
                 Scanner sc = new Scanner(System.in);
@@ -174,7 +100,6 @@ public class AddressBook {
                     System.out.println("\n             ~ ~ ~ ADD NEW CONTACT press 1-9  ::  To EXIT press (0 ZERO) ~ ~ ~             ");
                     exit = sc.nextInt();
                 }
-            }
             System.out.println("\n             ~ ~ ~ press 1 to 9 add onemore ADDRESS BOOK  ::  Exit ( 0 ZERO ) ~ ~ ~              ");
             getMoreBook = get.nextInt();
         } while (getMoreBook == 1);
